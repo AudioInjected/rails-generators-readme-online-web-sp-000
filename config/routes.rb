01f976fcb14a584e25007ace60f1342716bce472
Rafael Batista <rafaelb24@yahoo.com>
2019-03-20 17:31:12 +0000

    Automatically backed up by Learn

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'categories/show'
+
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  resources :categories
+end
